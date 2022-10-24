5b245d3ef2d36b16b9a7e5928c6735d1facc1c24
Davis Vaceris <davisvaceris@gmail.com>
2022-10-24 16:02:07 +0300

    store product position fixed

---
+++ js/store.js
@@ -0,0 +1,10 @@
+
+    var products = document.getElementsByClassName('product');
+    for (var i = 0; i < products.length;i++) {
+            products[i].classList.add('me-sm-3');
+            products[i].classList.add('me-0');
+            products[i].classList.add('col-md-3');
+            products[i].classList.add('col-sm-5');
+            products[i].classList.add('col-10');
+    }
+
