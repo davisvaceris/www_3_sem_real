41559fffa38c78634b2345e398a1ca77a9331126
Davis Vaceris <davisvaceris@gmail.com>
2023-01-10 12:37:33 +0200

    slider without content, with img

--- js/chat.js
+++ js/chat.js
@@ -1,5 +1,29 @@
-var txtarea = document.getElementById('txtarea')
-var send = document.getElementById('send');
 
-txtarea.style['height']="200px";
 
+// checks page 
+var htmlnamefull= document.URL;
+let htmlIndex = htmlnamefull.lastIndexOf("/")+1;
+var htmlname= htmlnamefull.substring(htmlIndex);
+switch (htmlname)
+{
+    case 'chat.html':
+        var txtarea = document.getElementById('txtarea')
+        var send = document.getElementById('send');
+        txtarea.style['height']="200px";
+
+    break;
+    case 'news.html':
+        var swiper = new Swiper(".mySwiper", {
+            pagination: {
+              el: ".swiper-pagination",
+              type: "progressbar",
+            },
+            navigation: {
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            },
+          });
+          
+          
+        break;
+}
